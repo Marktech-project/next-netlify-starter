de601de17a66cec4afc7d2642c29d9555757b8c9
Marktech Project <123752101+Marktech-project@users.noreply.github.com>
2023-02-11 23:15:33 +0100

    Add files via upload

---
+++ Project/app.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
+
+/*
+const hamburgerButton = document.querySelector(".nav-toggler")
+const navigation = document.querySelector("nav")
+
+hamburgerButton.addEventListener("click", toggleNav)
+
+function toggleNav(){
+  hamburgerButton.classList.toggle("active")
+  navigation.classList.toggle("active")
+}*/
